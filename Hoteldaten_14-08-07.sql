f06b729eb283a761d97766e54e3b6aa94fac2a82
Stefanie Lehmann <stefanie.lehmann-berlin@web.de>
2014-08-07 18:23:48 +0200

    erste Version

---
+++ Hoteldaten_14-08-07.sql
@@ -0,0 +1,122 @@
+﻿--Daten--
+
+--Hotels--
+INSERT INTO Hotel VALUES
+	(1, 'Hotel Budapest', 'Am schwarzen Weg 166, 66121 Traumstadt', 'Romantik'),
+	(2, 'The Glorious Inn', 'Kerzenweg 24, 48796 Lichterstadt', 'Business'),
+	(3, 'Hotel NoSleep', 'Loud Avenue 10, 59874 Elinoise', 'Romantik'),
+	(4, 'The Witz-Garlton', 'Schickimicki Allee 501, 68745 Werlin', 'Luxus'),
+	(5, 'The Monument', 'Auf dem Berg 15, 45921 Lexigton', 'Wellness'),
+	(6, 'My FairGround', 'Hinterm Wald, 14785 Oaktown', 'Budget'),
+	(7, 'The Octopus', 'Korallenriff 478, 87465 Seaside', 'Family'),
+	(8, 'The Hunter', 'Bounty 12, 58123 Huntersbay', 'Business'),
+	(9, 'KnightRider', 'Hasselhoof Road 66, 78945 The Hoff', 'Budget'),
+	(10, 'BudgetInn', 'Jansbillig 1, 00001 Pennyton', 'Budget'),
+	(11, 'The Shining', 'Scary Road 666, 66666 De Vilstown', 'Family');
+
+--Kunden--
+
+insert into Kunden (kid, erstellungszeitpunkt,vorname,nachname) values (1, now(),'Michelle','Hamilton');
+insert into Kunden (kid, erstellungszeitpunkt,vorname,nachname) values (2, now(), 'Adam','Frazier');
+insert into Kunden (kid, erstellungszeitpunkt,vorname,nachname) values (3, now(),'Jesse','King');
+insert into Kunden (kid, erstellungszeitpunkt,vorname,nachname) values (4, now(),'Howard','Cole');
+insert into Kunden (kid, erstellungszeitpunkt,vorname,nachname) values (5, now(),'Ashley','Gonzalez');
+insert into Kunden (kid, erstellungszeitpunkt,vorname,nachname) values (6, now(),'Russell','Palmer');
+insert into Kunden (kid, erstellungszeitpunkt,vorname,nachname) values (7, now(),'Aaron','Miller');
+insert into Kunden (kid, erstellungszeitpunkt,vorname,nachname) values (8, now(),'Linda','West');
+insert into Kunden (kid, erstellungszeitpunkt,vorname,nachname) values (9, now(),'Brandon','Rogers');
+insert into Kunden (kid, erstellungszeitpunkt,vorname,nachname) values (10, now(),'Daniel','Riley');
+insert into Kunden (kid, erstellungszeitpunkt,vorname,nachname) values (11, now(),'Kathryn','Richards');
+insert into Kunden (kid, erstellungszeitpunkt,vorname,nachname) values (12, now(),'Elizabeth','Lawson');
+insert into Kunden (kid, erstellungszeitpunkt,vorname,nachname) values (13, now(),'Ruby','Simmons');
+insert into Kunden (kid, erstellungszeitpunkt,vorname,nachname) values (14, now(),'Lillian','Brooks');
+insert into Kunden (kid, erstellungszeitpunkt,vorname,nachname) values (15, now(),'Ruby','Anderson');
+insert into Kunden (kid, erstellungszeitpunkt,vorname,nachname) values (16, now(),'Debra','Knight');
+insert into Kunden (kid, erstellungszeitpunkt,vorname,nachname) values (17, now(),'Debra','Wood');
+insert into Kunden (kid, erstellungszeitpunkt,vorname,nachname) values (18, now(),'Robert','Powell');
+insert into Kunden (kid, erstellungszeitpunkt,vorname,nachname) values (19, now(),'Maria','Weaver');
+insert into Kunden (kid, erstellungszeitpunkt,vorname,nachname) values (20, now(),'Thomas','Castillo');
+insert into Kunden (kid, erstellungszeitpunkt,vorname,nachname) values (21, now(),'Andrea','Richards');
+insert into Kunden (kid, erstellungszeitpunkt,vorname,nachname) values (22, now(),'Tina','Crawford');
+insert into Kunden (kid, erstellungszeitpunkt,vorname,nachname) values (23, now(),'Gerald','Olson');
+insert into Kunden (kid, erstellungszeitpunkt,vorname,nachname) values (24, now(),'Sean','Williams');
+insert into Kunden (kid, erstellungszeitpunkt,vorname,nachname) values (25, now(),'Stephanie','Kennedy');
+insert into Kunden (kid, erstellungszeitpunkt,vorname,nachname) values (26, now(),'Todd','Morris');
+insert into Kunden (kid, erstellungszeitpunkt,vorname,nachname) values (27, now(),'Julie','Kelly');
+insert into Kunden (kid, erstellungszeitpunkt,vorname,nachname) values (28, now(),'Susan','Richards');
+insert into Kunden (kid, erstellungszeitpunkt,vorname,nachname) values (29, now(),'Carolyn','Ruiz');
+insert into Kunden (kid, erstellungszeitpunkt,vorname,nachname) values (30, now(),'Henry','Price');
+insert into Kunden (kid, erstellungszeitpunkt,vorname,nachname) values (31, now(),'Eugene','Russell');
+insert into Kunden (kid, erstellungszeitpunkt,vorname,nachname) values (32, now(),'Sarah','Allen');
+insert into Kunden (kid, erstellungszeitpunkt,vorname,nachname) values (33, now(),'Jeremy','Cox');
+insert into Kunden (kid, erstellungszeitpunkt,vorname,nachname) values (34, now(),'Sarah','Griffin');
+insert into Kunden (kid, erstellungszeitpunkt,vorname,nachname) values (35, now(),'Paula','Sanchez');
+insert into Kunden (kid, erstellungszeitpunkt,vorname,nachname) values (36, now(),'Jeffrey','Perkins');
+insert into Kunden (kid, erstellungszeitpunkt,vorname,nachname) values (37, now(),'Barbara','Lee');
+insert into Kunden (kid, erstellungszeitpunkt,vorname,nachname) values (38, now(),'Carolyn','West');
+insert into Kunden (kid, erstellungszeitpunkt,vorname,nachname) values (39, now(),'Craig','Simmons');
+insert into Kunden (kid, erstellungszeitpunkt,vorname,nachname) values (40, now(),'Patricia','Miller');
+insert into Kunden (kid, erstellungszeitpunkt,vorname,nachname) values (41, now(),'Steve','Griffin');
+insert into Kunden (kid, erstellungszeitpunkt,vorname,nachname) values (42, now(),'Frances','Patterson');
+insert into Kunden (kid, erstellungszeitpunkt,vorname,nachname) values (43, now(),'Melissa','Myers');
+insert into Kunden (kid, erstellungszeitpunkt,vorname,nachname) values (44, now(),'Jeremy','Duncan');
+insert into Kunden (kid, erstellungszeitpunkt,vorname,nachname) values (45, now(),'Donald','Parker');
+insert into Kunden (kid, erstellungszeitpunkt,vorname,nachname) values (46, now(),'Betty','James');
+insert into Kunden (kid, erstellungszeitpunkt,vorname,nachname) values (47, now(),'Thomas','Fields');
+insert into Kunden (kid, erstellungszeitpunkt,vorname,nachname) values (48, now(),'Doris','Lawson');
+insert into Kunden (kid, erstellungszeitpunkt,vorname,nachname) values (49, now(),'John','Miller');
+insert into Kunden (kid, erstellungszeitpunkt,vorname,nachname) values (50, now(),'Gloria','Gomez');
+insert into Kunden (kid, erstellungszeitpunkt,vorname,nachname) values (51, now(),'Emily','Bishop');
+insert into Kunden (kid, erstellungszeitpunkt,vorname,nachname) values (52, now(),'Edward','Cruz');
+insert into Kunden (kid, erstellungszeitpunkt,vorname,nachname) values (53, now(),'Craig','Boyd');
+insert into Kunden (kid, erstellungszeitpunkt,vorname,nachname) values (54, now(),'Doris','Moore');
+insert into Kunden (kid, erstellungszeitpunkt,vorname,nachname) values (55, now(),'Nancy','Cole');
+insert into Kunden (kid, erstellungszeitpunkt,vorname,nachname) values (56, now(),'Wanda','Rivera');
+insert into Kunden (kid, erstellungszeitpunkt,vorname,nachname) values (57, now(),'Eric','Hill');
+insert into Kunden (kid, erstellungszeitpunkt,vorname,nachname) values (58, now(),'Joe','Hudson');
+insert into Kunden (kid, erstellungszeitpunkt,vorname,nachname) values (59, now(),'Brian','Mitchell');
+insert into Kunden (kid, erstellungszeitpunkt,vorname,nachname) values (60, now(),'Russell','Gibson');
+insert into Kunden (kid, erstellungszeitpunkt,vorname,nachname) values (61, now(),'Phillip','Sullivan');
+insert into Kunden (kid, erstellungszeitpunkt,vorname,nachname) values (62, now(),'Benjamin','Anderson');
+insert into Kunden (kid, erstellungszeitpunkt,vorname,nachname) values (63, now(),'Jeffrey','Lane');
+insert into Kunden (kid, erstellungszeitpunkt,vorname,nachname) values (64, now(),'Justin','Parker');
+insert into Kunden (kid, erstellungszeitpunkt,vorname,nachname) values (65, now(),'Barbara','Wells');
+insert into Kunden (kid, erstellungszeitpunkt,vorname,nachname) values (66, now(),'Diana','Cole');
+insert into Kunden (kid, erstellungszeitpunkt,vorname,nachname) values (67, now(),'Raymond','Cook');
+insert into Kunden (kid, erstellungszeitpunkt,vorname,nachname) values (68, now(),'Christine','Rice');
+insert into Kunden (kid, erstellungszeitpunkt,vorname,nachname) values (69, now(),'Lori','Diaz');
+insert into Kunden (kid, erstellungszeitpunkt,vorname,nachname) values (70, now(),'Janice','Bradley');
+insert into Kunden (kid, erstellungszeitpunkt,vorname,nachname) values (71, now(),'Wayne','Hill');
+insert into Kunden (kid, erstellungszeitpunkt,vorname,nachname) values (72, now(),'Melissa','Dunn');
+insert into Kunden (kid, erstellungszeitpunkt,vorname,nachname) values (73, now(),'Antonio','Hawkins');
+insert into Kunden (kid, erstellungszeitpunkt,vorname,nachname) values (74, now(),'Antonio','Gordon');
+insert into Kunden (kid, erstellungszeitpunkt,vorname,nachname) values (75, now(),'Michael','Bailey');
+insert into Kunden (kid, erstellungszeitpunkt,vorname,nachname) values (76, now(),'Harry','Sanders');
+insert into Kunden (kid, erstellungszeitpunkt,vorname,nachname) values (77, now(),'Heather','Gilbert');
+insert into Kunden (kid, erstellungszeitpunkt,vorname,nachname) values (78, now(),'Craig','Butler');
+insert into Kunden (kid, erstellungszeitpunkt,vorname,nachname) values (79, now(),'Edward','Lawson');
+insert into Kunden (kid, erstellungszeitpunkt,vorname,nachname) values (80, now(),'Judy','Hudson');
+insert into Kunden (kid, erstellungszeitpunkt,vorname,nachname) values (81, now(),'Gregory','Marshall');
+insert into Kunden (kid, erstellungszeitpunkt,vorname,nachname) values (82, now(),'Pamela','Lopez');
+insert into Kunden (kid, erstellungszeitpunkt,vorname,nachname) values (83, now(),'Jeffrey','Mills');
+insert into Kunden (kid, erstellungszeitpunkt,vorname,nachname) values (84, now(),'Martha','Gray');
+insert into Kunden (kid, erstellungszeitpunkt,vorname,nachname) values (85, now(),'Sandra','Flores');
+insert into Kunden (kid, erstellungszeitpunkt,vorname,nachname) values (86, now(),'Philip','Burke');
+insert into Kunden (kid, erstellungszeitpunkt,vorname,nachname) values (87, now(),'Gary','Flores');
+insert into Kunden (kid, erstellungszeitpunkt,vorname,nachname) values (88, now(),'Carol','Cole');
+insert into Kunden (kid, erstellungszeitpunkt,vorname,nachname) values (89, now(),'Carolyn','Reed');
+insert into Kunden (kid, erstellungszeitpunkt,vorname,nachname) values (90, now(),'John','Dunn');
+insert into Kunden (kid, erstellungszeitpunkt,vorname,nachname) values (91, now(),'Earl','Wilson');
+insert into Kunden (kid, erstellungszeitpunkt,vorname,nachname) values (92, now(),'Doris','Washington');
+insert into Kunden (kid, erstellungszeitpunkt,vorname,nachname) values (93, now(),'Carol','Martinez');
+insert into Kunden (kid, erstellungszeitpunkt,vorname,nachname) values (94, now(),'Virginia','Lawrence');
+insert into Kunden (kid, erstellungszeitpunkt,vorname,nachname) values (95, now(),'Deborah','Russell');
+insert into Kunden (kid, erstellungszeitpunkt,vorname,nachname) values (96, now(),'Bonnie','Pierce');
+insert into Kunden (kid, erstellungszeitpunkt,vorname,nachname) values (97, now(),'Jose','Reynolds');
+insert into Kunden (kid, erstellungszeitpunkt,vorname,nachname) values (98, now(),'Steve','Dunn');
+insert into Kunden (kid, erstellungszeitpunkt,vorname,nachname) values (99, now(),'Laura','Franklin');
+insert into Kunden (kid, erstellungszeitpunkt,vorname,nachname) values (100, now(),'Arthur','Green');
+insert into Kunden (kid, erstellungszeitpunkt,vorname,nachname) values (101, now(),'Stefanie','Lehmann');
+insert into Kunden (kid, erstellungszeitpunkt,vorname,nachname) values (102, now(),'Elisabeth','Pich');
+
+--
---
+++ Hotelverwaltungssystem_14.08.06.sql
@@ -0,0 +1,446 @@
+﻿--Hotelverwaltugssystem
+
+--Domaenen
+
+CREATE DOMAIN HOTELTYP varchar
+	CHECK (	VALUE = 'Romantik' OR 
+		VALUE = 'Business' OR 
+		VALUE = 'Family' OR 
+		VALUE = 'Budget' OR
+		VALUE = 'Wellness' OR
+		VALUE = 'Luxus' );
+
+CREATE DOMAIN OEFFNUNGSZEIT varchar
+	CHECK (	VALUE = 'MO-FR 07/17' OR
+		VALUE = 'MO-SO 07/17' OR 
+		VALUE = 'MO-SO 07/23' OR
+		VALUE = '24/7' OR 
+		VALUE = 'MO-SO 18/03' );
+
+CREATE DOMAIN BESONDERHEIT varchar
+	CHECK ( VALUE = 'vegan' OR
+		VALUE = 'vegetarisch' or
+		VALUE = 'Lactose-Intoleranz' OR
+		VALUE = 'Ei-Allergie' OR
+		VALUE = 'kosher' OR
+		VALUE = 'halal');
+
+CREATE DOMAIN PLATZART varchar
+	CHECK (	VALUE = 'Rasenplatz' OR 
+		VALUE = 'Asphaltplatz' OR
+		VALUE = 'Gummiplatz' OR
+		VALUE = 'Astroturf' );
+
+CREATE DOMAIN KUECHE varchar
+	CHECK (	VALUE = 'italienisch' OR 
+		VALUE = 'chinesisch' OR
+		VALUE = 'vegetarisch' OR
+		VALUE = 'deutsch' OR
+		VALUE = 'rustikal' OR
+		VALUE = 'japanish' OR
+		VALUE = 'urbanisch' OR
+		VALUE = 'organic' );
+
+CREATE DOMAIN AUSRUESTUNGSTYP varchar
+	CHECK (	VALUE = 'Schlaeger' OR 
+		VALUE = 'Helm' OR
+		VALUE = 'Golfwagen' OR
+		VALUE = 'Knieschoner' OR
+		VALUE = 'Golfhanschuhe' OR
+		VALUE = 'Golftasche' );
+
+CREATE DOMAIN MENUKATEGORIE varchar
+	CHECK (	VALUE = 'Softgetraenk' OR 
+		VALUE = 'Vorspeise' OR
+		VALUE = 'Hauptspeise' OR
+		VALUE = 'Nachspeise' OR
+		VALUE = 'Cocktail' OR
+		VALUE = 'Longdrink' OR
+		VALUE = 'Likoer' OR
+		VALUE = 'Hochprozetiges' OR
+		VALUE = 'Heissgetraenk' OR
+		VALUE = 'Snack' OR
+		VALUE = 'Salat' OR
+		VALUE = 'Beilage' OR
+		VALUE = 'Lowfat' OR
+		VALUE = 'Kinderkarte' OR
+		VALUE = 'Seniors');
+
+CREATE DOMAIN GAESTESTATUS varchar
+	CHECK ( VALUE = 'ARRIVAL' OR	--am Tag der Anreise
+		VALUE = 'RESERVED' OR 	-- normale Reservierung
+		VALUE = 'CANCELED' OR 	-- storniert, war reserved
+		VALUE = 'IN-HOUSE' OR	--im Haus eingecheckt
+		VALUE = 'CHECKED-OUT' OR --abgereist
+		VALUE = 'TURN-DOWN' ) ;	-- Reservierung nicht angenommen (Ablehnung)
+
+CREATE DOMAIN VERPFLEGUNGSSTUFE varchar
+	CHECK ( VALUE = 'ROOM' OR 	--nur Zimmer
+		VALUE = 'BRFST' OR	--Fruehstueck
+		VALUE = 'HBL' OR 	--Halbpension Mittag (half board lunch)
+		VALUE = 'HBD' OR 	--Halbpension Abends (half bord dinner)
+		VALUE = 'FB' OR 	--Vollpension (full board)
+		VALUE = 'ALL'); 	--All inclusive
+
+CREATE DOMAIN ZIMMERKATEGORIE varchar
+	CHECK ( VALUE = 'EZMM' OR	--Einzelzmmer mit Meerblick
+		VALUE = 'EZOM' OR	--Einzelzimmer ohne Meerblick
+		VALUE = 'DZMM' OR	--Doppelzimmer mit Meerblick
+		VALUE = 'DZOM' OR	--Doppelzimmer ohne Meerblick
+		VALUE = 'TRMM' OR	--Drei-Bett-Zimmer mit Meerblick
+		VALUE = 'TROM' OR	--Drei-Bett-Zimmer ohne Meerblick
+		VALUE = 'SUIT');
+
+
+--Hotel
+
+CREATE TABLE Hotel (
+	HotelID int NOT NULL,
+	Hotelname varchar NOT NULL,
+	Adresse varchar NOT NULL,
+	Hoteltyp HOTELTYP NOT NULL,
+
+	PRIMARY KEY (HotelID),
+	UNIQUE (HotelID)
+);
+
+CREATE TABLE Kunden(
+	KID int NOT NULL, 
+	Erstellungszeitpunkt timestamp DEFAULT now()NOT NULL,
+	Telefonnummer int,
+	Kreditkarte int,
+	Besonderheiten BESONDERHEIT,
+	Nachname varChar NOT NULL,
+	Vorname varChar NOT NULL,
+	Adresse varChar,
+	VIP boolean DEFAULT false,
+
+	PRIMARY KEY (KID),
+	UNIQUE (KID)
+);
+
+CREATE TABLE Zimmer (
+	gehoertZuHotel int NOT NULL, 
+	Zimmernummer int NOT NULL,
+	Zimmerkategorie ZIMMERKATEGORIE NOT NULL,
+	Dreckig boolean NOT NULL,
+	OutofOrder boolean NOT NULL,
+	maxPersonen int NOT NULL,
+
+	FOREIGN KEY (gehoertZuHotel) REFERENCES Hotel,
+	PRIMARY KEY (gehoertZuHotel,Zimmernummer)
+
+);
+
+CREATE TABLE Abteilung(
+	gehoertZuHotel int NOT NULL,
+	AID int NOT NULL,
+	Oeffnungszeiten OEFFNUNGSZEIT NOT NULL,
+	Location varchar NOT NULL,
+	Name varchar NOT NULL,
+
+	FOREIGN KEY (gehoertZuHotel) REFERENCES Hotel,
+	PRIMARY KEY(gehoertZuHotel, AID)
+);
+
+CREATE TABLE Sporteinrichtungen (
+	gehoertzuHotel int NOT NULL,
+	AID int NOT NULL,
+	Oeffnungszeiten OEFFNUNGSZEIT NOT NULL,
+	Location varchar NOT NULL,
+	Name varchar NOT NULL,
+	Preis money NOT NULL,
+
+	FOREIGN Key(gehoertZuHotel) REFERENCES Hotel,
+	PRIMARY KEY(AID, gehoertZuHotel)
+);
+
+CREATE TABLE Fahrraeder (
+	gehoertzuHotel int NOT NULL,
+	AID int NOT NULL,
+	Oeffnungszeiten OEFFNUNGSZEIT NOT NULL,
+	Location varchar NOT NULL,
+	Name varchar NOT NULL,
+	Preis money NOT NULL,
+	Gaenge int NOT NULL,
+	Modell varchar,
+
+	FOREIGN KEY (gehoertZuHotel) REFERENCES Hotel,
+	PRIMARY KEY(AID, gehoertZuHotel)
+);
+
+CREATE TABLE Tennisplaetze (
+	gehoertzuHotel int NOT NULL,
+	AID int NOT NULL,
+	Oeffnungszeiten OEFFNUNGSZEIT NOT NULL,
+	Location varchar NOT NULL,
+	Name varchar NOT NULL,
+	Preis money NOT NULL,
+	Platzart PLATZART,
+
+	FOREIGN KEY (gehoertZuHotel) REFERENCES Hotel,
+	PRIMARY KEY(AID, gehoertZuHotel)
+);
+
+CREATE TABLE Golf (
+	gehoertzuHotel int NOT NULL,
+	AID int NOT NULL,
+	Oeffnungszeiten OEFFNUNGSZEIT NOT NULL,
+	Location varchar NOT NULL,
+	Name varchar NOT NULL,
+	Preis money NOT NULL,
+	Schwierigkeitsgrad int,
+	
+	FOREIGN KEY (gehoertZuHotel) REFERENCES Hotel,
+	PRIMARY KEY(AID, gehoertZuHotel)
+);
+
+CREATE TABLE Minigolf (
+	gehoertzuHotel int NOT NULL,
+	AID int NOT NULL,
+	Oeffnungszeiten OEFFNUNGSZEIT NOT NULL,
+	Location varchar NOT NULL,
+	Name varchar NOT NULL,
+	Preis money NOT NULL,
+	Schwierigkeitsgrad int,
+	
+	FOREIGN KEY (gehoertZuHotel) REFERENCES Hotel,
+	PRIMARY KEY(AID, gehoertZuHotel)
+);
+
+CREATE TABLE mieten (
+	KID int NOT NULL REFERENCES Kunden,
+	Zeitpunkt timestamp,
+	von timestamp NOT NULL,
+	bis timestamp NOT NULL,
+	gehoertZuHotel int NOT NULL,
+	AID int NOT NULL ,
+
+	
+	FOREIGN KEY (gehoertZuHotel, AID) REFERENCES Sporteinrichtungen,
+	FOREIGN KEY (KID) REFERENCES Kunden,
+	PRIMARY KEY (von, AID, gehoertZuHotel)
+);
+
+CREATE TABLE Schwimmbad (
+	gehoertZuHotel int REFERENCES Hotel,
+	AID int NOT NULL,
+	Oeffnugszeiten OEFFNUNGSZEIT NOT NULL,
+	Location varchar NOT NULL,
+	Name varchar NOT NULL,
+	LaengeBecken int NOT NULL,
+	Sauna boolean,
+	Preis money,
+
+	
+	PRIMARY KEY (gehoertZuHotel,AID)
+);
+
+CREATE TABLE Ausruestung (
+	Ausruestungstyp AUSRUESTUNGSTYP NOT NULL,
+	Ausruestungsnummer int NOT NULL,
+	gehoertZuSporteinrichtung int NOT NULL,
+	gehoertZuHotel int REFERENCES Hotel,
+
+	FOREIGN KEY (gehoertZuSporteinrichtung, gehoertzuHotel) REFERENCES Sporteinrichtungen,
+	PRIMARY KEY (Ausruestungstyp ,Ausruestungsnummer,
+	gehoertZuSporteinrichtung,
+	gehoertZuHotel)
+);
+
+CREATE TABLE leihen (
+	KID int NOT NULL REFERENCES Kunden, 
+	Ausruestungstyp AUSRUESTUNGSTYP NOT NULL,
+	Ausruestungsnummer int NOT NULL,
+	gehoertZuSporteinrichtung int NOT NULL, 
+	gehoertZuHotel int NOT NULL,
+	von timestamp NOT NULL,
+	bis timestamp NOT NULL,
+
+	FOREIGN KEY (Ausruestungstyp, Ausruestungsnummer, gehoertZuSporteinrichtung, gehoertZuHotel) REFERENCES Ausruestung,
+	PRIMARY KEY (Ausruestungstyp,Ausruestungsnummer, gehoertZuSporteinrichtung, gehoertZuHotel,von)
+
+);
+
+
+
+CREATE TABLE benutzen (
+
+	KID int NOT NULL, 
+	gehoertZuHotel int NOT NULL,
+	AID int NOT NULL,
+	von timestamp NOT NULL,
+	bis timestamp NOT NULL,
+
+	FOREIGN KEY (KID) REFERENCES Kunden,
+	PRIMARY KEY (KID,von)
+);
+
+CREATE TABLE Restauration  (
+	gehoertZuHotel int NOT NULL REFERENCES Hotel,
+	AID int NOT NULL,
+	Oeffnungszeiten Oeffnungszeit,
+	Location varChar,
+	Name varChar,
+	
+	PRIMARY KEY (gehoertZuHotel, AID)
+);
+
+CREATE TABLE Hotelbar  (
+	gehoertZuHotel int NOT NULL REFERENCES Hotel,
+	AID int NOT NULL,
+	Oeffnungszeiten Oeffnungszeit NOT NULL,
+	Location varChar NOT NULL,
+	Name varChar NOT NULL,
+
+	PRIMARY KEY (gehoertZuHotel, AID)
+);
+
+CREATE TABLE Restaurant  (
+	gehoertZuHotel int NOT NULL REFERENCES Hotel,
+	AID int NOT NULL,
+	Oeffnungszeiten Oeffnungszeit NOT NULL,
+	Location varChar NOT NULL,
+	Name varChar NOT NULL,
+	Sterne int NOT NULL,
+	Kueche Kueche NOT NULL,
+
+	 PRIMARY KEY (gehoertZuHotel, AID)
+);
+
+CREATE TABLE SpeisenUndGetraenke(
+	SpeiseID int NOT NULL ,
+	Name varChar NOT NULL,
+	Preis money NOT NULL,
+	Menukategorie MENUKATEGORIE NOT NULL,
+
+	PRIMARY KEY (SpeiseID)	
+);
+
+CREATE TABLE Essen(
+	SpeiseID int,
+	Name varChar,
+	Preis money NOT NULL,
+	Menukategorie Menukategorie NOT NULL,
+	Besonderheit Besonderheit,
+
+	PRIMARY KEY (SpeiseID)	
+);
+
+CREATE TABLE Trinken(
+	SpeiseID int NOT NULL,
+	Name varChar NOT NULL,
+	Preis money NOT NULL,
+	Menukategorie Menukategorie NOT NULL,
+	Alkoholgehalt numeric,
+	
+	PRIMARY KEY (SpeiseID)	
+);
+
+
+CREATE TABLE wirdServiertIn(
+	SpeiseID int NOT NULL ,
+	gehoertZuHotel int NOT NULL, 
+	AID int NOT NULL,
+
+	FOREIGN KEY(SpeiseID) REFERENCES SpeisenUndGetraenke,
+	FOREIGN KEY(AID, gehoertZuHotel) REFERENCES Restauration
+);
+
+
+
+CREATE TABLE konsumieren(
+	KID int NOT NULL,
+	SpeiseID int NOT NULL, 
+	Zeitpunkt timestamp NOT NULL,
+
+	FOREIGN KEY (KID) REFERENCES Kunden,
+	PRIMARY KEY (KID, Zeitpunkt)
+	
+);
+
+	
+CREATE TABLE Reservierungen(
+	Reservierungsnummer int NOT NULL, --eventuell serial hier
+	Zimmerpreis money NOT NULL,
+	Stornierungsnummer int,
+	Verpflegungsstufe VERPFLEGUNGSSTUFE NOT NULL,
+	Zimmerkategorie ZIMMERKATEGORIE,
+	Anreise date NOT NULL,
+	Abreise date NOT NULL,
+	Gaestestatus GAESTESTATUS,
+	Wuensche varchar,
+	Personenanzahl int NOT NULL,
+	Reservierungszeitpunkt timestamp NOT NULL,
+	reserviertVonKunde int NOT NULL,
+	zugewiesenesZimmer int,
+	ZimmerInHotel int,
+
+	FOREIGN KEY (reserviertVonKunde) REFERENCES Kunden,
+	FOREIGN KEY (zugewiesenesZimmer, ZimmerInHotel) REFERENCES Zimmer,
+	UNIQUE (Reservierungsnummer),
+	UNIQUE (Stornierungsnummer),
+	UNIQUE (zugewiesenesZimmer,ZimmerInHotel ,Anreise),
+	PRIMARY KEY  (Reservierungsnummer)
+);
+
+CREATE TABLE Ablehnungen(
+	Reservierungsnummer int NOT NULL,
+	Zimmerpreis money NOT NULL,
+	Stornierungsnummer int DEFAULT NULL,
+	Verpflegungsstufe Verpflegungsstufe,
+	Zimmerkategorie Zimmerkategorie,
+	Anreise date NOT NULL,
+	Abreise date NOT NULL,
+	Gaestestatus Gaestestatus,
+	Wuensche varchar,
+	Personenanzahl int,
+	Reservierungszeitpunkt timestamp NOT NULL,
+	reserviertVonKunde int,
+	zugewiesenesZimmer int,
+	ZimmerInHotel int,
+	Grund varchar,
+	Ablehnungszeitpunkt timestamp,
+	
+	FOREIGN KEY (reserviertVonKunde) REFERENCES Kunden,
+	FOREIGN KEY (zugewiesenesZimmer, ZimmerInHotel) REFERENCES Zimmer,
+	UNIQUE (Reservierungsnummer),
+	UNIQUE (Stornierungsnummer),
+	PRIMARY KEY  (Reservierungsnummer)
+);
+
+CREATE TABLE bezahlen (
+	Reservierungsnummer int REFERENCES Reservierungen NOT NULL,
+	KID int REFERENCES Kunden NOT NULL,
+	Zeitpunkt timestamp,
+
+	PRIMARY KEY (Reservierungsnummer)
+);
+
+
+CREATE TABLE Zimmerkarte (
+	KartenID int NOT NULL,
+	gesperrt boolean,
+
+	PRIMARY KEY (KartenID)
+);
+
+CREATE TABLE oeffnet (
+	KartenID int NOT NULL,
+	Zeitpunkt timestamp NOT NULL,
+	Zimmernummer int,
+	ZimmerInHotel int REFERENCES Hotel,
+	
+	FOREIGN KEY (Zimmernummer, ZimmerInHotel) REFERENCES Zimmer,
+	FOREIGN KEY (KartenID) REFERENCES Zimmerkarte,
+	PRIMARY KEY (KartenID, Zeitpunkt)
+);
+
+CREATE TABLE erhalten (
+	KundenID int REFERENCES Kunden,
+	KartenID int ,
+	Reservierungsnummer  int REFERENCES Reservierungen,
+	FOREIGN KEY (KartenID) REFERENCES Zimmerkarte
+);
+
+
