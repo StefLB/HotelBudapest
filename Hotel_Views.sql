d80533da9d57ed3d8030fd04baafb32dbcec801b
Elli <s9elpich@stud.uni-saarland.de>
2014-08-08 09:43:54 +0200

    Views and Trigger Dateien angelegt

---
+++ Hotel_Views.sql
@@ -0,0 +1,28 @@
+﻿-- Views
+
+-- belegteZimmerView: Belegt Zimmer, sortiert nach Hotel und Zimmernummer
+CREATE OR REPLACE VIEW belegteZimmerView AS
+SELECT zugewiesenesZimmer, ZimmerInHotel, dreckig
+	FROM Reservierungen JOIN Zimmer ON (zugewiesenesZimmer = Zimmernummer AND  ZimmerInHotel = gehoertZuHotel)
+	WHERE Gaestestatus = 'IN-HOUSE'
+	ORDER BY gehoertZuHotel ASC, ZimmerNummer ASC;
+
+-- Reinigungspersonal
+-- Zeigt Zimmer an, die vom Personal gereinigt werden muessen, sortiert nach Hotel und Zimmernummer
+CREATE OR REPLACE VIEW ReinigungspersonalView AS 
+SELECT gehoertZuHotel, Zimmernummer
+FROM Zimmer
+WHERE dreckig
+ORDER BY gehoertZuHotel ASC, ZimmerNummer ASC;
+
+-- HotelManager 
+
+
+
+
+-- Freie Sportplaetze
+-- Ein Gast moechte sehen, welche Sportplaetze am jetzigen Tag noch zu vermieten sind
+CREATE OR REPLACE VIEW 
+
+
+
