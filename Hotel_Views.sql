216c461a91e0454bcf7fdee6c92979c1e39a56d8
Elli <s9elpich@stud.uni-saarland.de>
2014-08-08 14:27:49 +0200

    todo Views notiert

--- Hotel_Views.sql
+++ Hotel_Views.sql
@@ -1,13 +1,16 @@
-﻿-- Views
+﻿-- VIEWS
 
--- belegteZimmerView: Belegt Zimmer, sortiert nach Hotel und Zimmernummer
+-- 
+
+-- belegteZimmerView
+-- Belegt Zimmer, sortiert nach Hotel und Zimmernummer
 CREATE OR REPLACE VIEW belegteZimmerView AS
 SELECT zugewiesenesZimmer, ZimmerInHotel, dreckig
 	FROM Reservierungen JOIN Zimmer ON (zugewiesenesZimmer = Zimmernummer AND  ZimmerInHotel = gehoertZuHotel)
 	WHERE Gaestestatus = 'IN-HOUSE'
 	ORDER BY gehoertZuHotel ASC, ZimmerNummer ASC;
 
--- Reinigungspersonal
+-- ReinigungspersonalView
 -- Zeigt Zimmer an, die vom Personal gereinigt werden muessen, sortiert nach Hotel und Zimmernummer
 CREATE OR REPLACE VIEW ReinigungspersonalView AS 
 SELECT gehoertZuHotel, Zimmernummer
@@ -20,9 +23,19 @@ ORDER BY gehoertZuHotel ASC, ZimmerNummer ASC;
 
 
 
--- Freie Sportplaetze
+
+-- UnbezahlteReservierungView
+
+
+
+-- GourmetGastView
+-- Ein Gast moechte alle Hotel Restaurants angezeigt bekommen die mehr als 3 Sterne haben
+CREATE OR REPLACE VIEW GourmetGastView AS 
+
+
+-- FreieSportplaetzeView 
 -- Ein Gast moechte sehen, welche Sportplaetze am jetzigen Tag noch zu vermieten sind
-CREATE OR REPLACE VIEW 
+CREATE OR REPLACE VIEW FreieSportplaetzeView
 
 
 
