c6764321cd4762bd6910b3127144bc392ed4c737
Stefanie Lehmann <stefanie.lehmann-berlin@web.de>
2014-08-10 10:12:56 +0200

    ManagerView mit Zimmer / freieZimmerView verkürzt

--- Hotel_Views.sql
+++ Hotel_Views.sql
@@ -3,116 +3,16 @@
 -- freieZimmerView 
 -- zeigt Hotels an, die noch freie Zimmer haben, mit Kategorie und Anzahlzimmer in Kategorie
 CREATE OR REPLACE VIEW freieZimmerView AS
-SELECT hotelid,COALESCE(ezmm,0) as ezmm,COALESCE(ezom,0) as ezom,COALESCE(dzmm,0) as dzmm ,COALESCE(dzom,0) as dzom,COALESCE (suit,0) as suit,COALESCE(trmm,0) as trmm,COALESCE(trom,0)as trom
+SELECT hotelid, count(CASE WHEN zimmerkategorie='EZOM' THEN 1 ELSE NULL END) as ezom, count(CASE WHEN zimmerkategorie='EZMM' THEN 1 ELSE NULL END) as ezmm, count(CASE WHEN zimmerkategorie='DZOM' THEN 1 ELSE NULL END) as dzom, count(CASE WHEN zimmerkategorie='DZMM' THEN 1 ELSE NULL END) as dzmm, count(CASE WHEN zimmerkategorie='TROM' THEN 1 ELSE NULL END) as trom, count(CASE WHEN zimmerkategorie='TRMM' THEN 1 ELSE NULL END) as trmm, count(CASE WHEN zimmerkategorie='SUIT' THEN 1 ELSE NULL END) as suit
 FROM
-
-(SELECT hotelid from hotel) AS HOTELSA
-LEFT OUTER JOIN
-(WITH B AS  (
-WITH A AS (SELECT *
+(WITH BelegtenZimmer AS (SELECT *
 	FROM reservierungen
 	WHERE Anreise=current_date OR Gaestestatus = 'IN-HOUSE')
-SELECT zimmer.gehoertzuhotel as hotela, zimmer.zimmerkategorie
-	FROM zimmer LEFT OUTER JOIN A
-	 ON (zimmernummer = zimmer AND zimmer.gehoertzuhotel = A.gehoertzuhotel)
-	 ORDER By hotela ASC)	 
-SELECT B.hotela, count(zimmerkategorie) as EZMM
-FROM B
-WHERE zimmerkategorie = 'EZMM'
-GROUP BY B.hotela) as C
-on hotelid=hotela
-LEFT OUTER JOIN
-(WITH E AS  (
-WITH D AS (SELECT *
-	FROM reservierungen
-	WHERE Anreise=current_date OR Gaestestatus = 'IN-HOUSE' )
-SELECT zimmer.gehoertzuhotel as hotelB, zimmer.zimmerkategorie
-	FROM zimmer LEFT OUTER JOIN D
-	 ON (zimmernummer = zimmer AND zimmer.gehoertzuhotel = D.gehoertzuhotel)
-	 ORDER By hotelB ASC)	 
-SELECT E.hotelB, count(zimmerkategorie) as EZOM
-FROM E
-WHERE zimmerkategorie = 'EZOM'
-GROUP BY E.hotelB) AS F
-on hotelid=hotelb
-LEFT OUTER JOIN
-
-(WITH Z AS  (
-WITH Y AS (SELECT *
-	FROM reservierungen
-	WHERE Anreise=current_date OR Gaestestatus = 'IN-HOUSE' )
-SELECT zimmer.gehoertzuhotel as hotelC, zimmer.zimmerkategorie
-	FROM zimmer LEFT OUTER JOIN Y
-	 ON (zimmernummer = zimmer AND zimmer.gehoertzuhotel = Y.gehoertzuhotel)
-	 ORDER By hotelC ASC)	 
-SELECT Z.hotelC, count(zimmerkategorie) as DZMM
-FROM Z
-WHERE zimmerkategorie = 'DZMM'
-GROUP BY Z.hotelC) AS G
-ON hotelid=hotelc
-
-LEFT OUTER JOIN
-(WITH X AS  (
-WITH V AS (SELECT *
-	FROM reservierungen
-	WHERE Anreise=current_date OR Gaestestatus = 'IN-HOUSE' )
-SELECT zimmer.gehoertzuhotel as hotelD, zimmer.zimmerkategorie
-	FROM zimmer LEFT OUTER JOIN V
-	 ON (zimmernummer = zimmer AND zimmer.gehoertzuhotel = V.gehoertzuhotel)
-	 ORDER BY hotelD ASC)	 
-SELECT X.hotelD, count(zimmerkategorie) as DZOM
-FROM X
-WHERE zimmerkategorie = 'DZOM'
-GROUP BY X.hotelD) AS H
-ON hotelid=hoteld
-
-LEFT OUTER JOIN
-
-(WITH O AS  (
-WITH P AS (SELECT *
-	FROM reservierungen
-	WHERE Anreise=current_date OR Gaestestatus = 'IN-HOUSE' )
-SELECT zimmer.gehoertzuhotel AS hotelF , zimmer.zimmerkategorie
-	FROM zimmer LEFT OUTER JOIN P
-	 ON (zimmernummer = zimmer AND zimmer.gehoertzuhotel = P.gehoertzuhotel)
-	 ORDER By hotelF  ASC)	 
-SELECT O.hotelF , count(zimmerkategorie) as SUIT
-FROM O
-WHERE zimmerkategorie = 'SUIT'
-GROUP BY O.hotelF ) AS I
-on hotelid=hotelf
-
-LEFT OUTER JOIN
-
-(WITH Q AS  (
-WITH R AS (SELECT *
-	FROM reservierungen
-	WHERE Anreise=current_date OR Gaestestatus = 'IN-HOUSE' )
-SELECT zimmer.gehoertzuhotel as hotelE, zimmer.zimmerkategorie
-	FROM zimmer LEFT OUTER JOIN R
-	 ON (zimmernummer = zimmer AND zimmer.gehoertzuhotel = R.gehoertzuhotel)
-	 ORDER By hotelE ASC)	 
-SELECT Q.hotelE, count(zimmerkategorie) as TRMM
-FROM Q
-WHERE zimmerkategorie = 'TRMM'
-GROUP BY Q.hotelE) AS J
-ON hotelid = hotelE
-
-LEFT OUTER JOIN
-
-(WITH T AS  (
-WITH U AS (SELECT *
-	FROM reservierungen
-	WHERE Anreise=current_date OR Gaestestatus = 'IN-HOUSE' )
-SELECT zimmer.gehoertzuhotel as hotelG, zimmer.zimmerkategorie
-	FROM zimmer LEFT OUTER JOIN U
-	 ON (zimmernummer = zimmer AND zimmer.gehoertzuhotel = U.gehoertzuhotel)
-	 ORDER By hotelG ASC)	 
-SELECT T.hotelG, count(zimmerkategorie) as TROM
-FROM T
-WHERE zimmerkategorie = 'TROM'
-GROUP BY T.hotelG) as K
-on hotelid = hotelg;
+SELECT zimmer.gehoertzuhotel as hotelid, zimmer.zimmerkategorie
+	FROM zimmer LEFT OUTER JOIN BelegtenZimmer
+	 ON (zimmernummer = zimmer AND zimmer.gehoertzuhotel = BelegtenZimmer.gehoertzuhotel)
+	 ORDER By hotelid ASC) freieZimmer
+GROUP BY hotelid;
 
 -- belegteZimmerView
 -- Belegte Zimmer, sortiert nach Hotel und Zimmernummer
@@ -133,7 +33,7 @@ ORDER BY ZimmerInHotel ASC, zugewiesenesZimmer ASC;
 -- HotelManager 
 -- Hotels sortiert nach Umsatz, mit dazugehoerigen Bars sortiert nach Umsatz, dazu die beliebteste Zimmerkategorie
 CREATE OR REPLACE VIEW HotelmanagerView AS
-SELECT hotelid, (umsatzrooms+konsum+mieteinnahmen+benutzteinnahmen) as gesamtumsatz
+SELECT hotelid, (umsatzrooms+konsum+mieteinnahmen+benutzteinnahmen) as gesamtumsatz, COALESCE (ezom, 0)as ezom, COALESCE(ezmm,0) as ezmm, COALESCE(dzom,0) as dzom, COALESCE(dzmm,0) as dzmm, COALESCE (trom,0) as trom , COALESCE (trmm,0) as trmm, COALESCE (suit,0) as suit
 FROM
 (SELECT hotelid,COALESCE (umsatzrooms, '0,00€') as umsatzrooms, COALESCE(konsum, '0,00 €') as konsum , COALESCE(mieteneinahmen, '0,00 €') as mieteinnahmen , COALESCE (benutzteinnahmen, '0,00 €') as benutzteinnahmen 
 FROM (SELECT hotelid from hotel) as hotels
@@ -200,7 +100,19 @@ JOIN schwimmbad
 ON schwimmbad.aid = Benutzt.aid
 GROUP BY hotelbe) as Benutzen --extrabenutzen 
 
-ON hotelid=hotelbe) as Umsatzkomplett
+ON hotelid=hotelbe)as Gesamtumsatz--GEsamtumsatz
+
+
+LEFT OUTER JOIN
+
+(SELECT gehoertzuhotel as wo, count(CASE WHEN zimmerkategorie='EZOM' THEN 1 ELSE NULL END) as ezom, count(CASE WHEN zimmerkategorie='EZMM' THEN 1 ELSE NULL END) as ezmm, count(CASE WHEN zimmerkategorie='DZOM' THEN 1 ELSE NULL END) as dzom, count(CASE WHEN zimmerkategorie='DZMM' THEN 1 ELSE NULL END) as dzmm, count(CASE WHEN zimmerkategorie='TROM' THEN 1 ELSE NULL END) as trom, count(CASE WHEN zimmerkategorie='TRMM' THEN 1 ELSE NULL END) as trmm, count(CASE WHEN zimmerkategorie='SUIT' THEN 1 ELSE NULL END) as suit
+FROM reservierungen
+WHERE gaestestatus = 'CHECKED-OUT' OR gaestestatus = 'IN-HOUSE'
+GROUP BY wo
+ORDER BY wo) as TOTALGebuchteZImmer --alle kategorieszimmerreservierungen checked-out und in-house
+on hotelid=wo;
+
+
 
 
 
