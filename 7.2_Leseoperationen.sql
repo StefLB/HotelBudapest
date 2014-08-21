ee804f17cb82c3949e37d384b1b5d9f2dc7a17b6
Elli <s9elpich@stud.uni-saarland.de>
2014-08-21 20:40:48 +0200

    sieht gut aus

--- 7.2_Leseoperationen.sql
+++ 7.2_Leseoperationen.sql
@@ -20,7 +20,7 @@ der Gaeste holen, die im Haus sind.
 5. 'Welches Hotel habe ich heute nochmal gebucht', fragt die viel reisende Frau 'Hamilton' in der zentralen Reservierung nach.
 Bevor die Reservierungsmitabrieterin die Daten nennt, gleicht sie nochmal die persoenlichen Daten mit Frau 'Hamilton' ab.
 
-6. Das Flugzeug 'Boring Airline' musste seinen Flug aufgrund von schlechten WEtterbedingngen stornieren.
+6. Das Flugzeug 'Boring Airline' musste seinen Flug aufgrund von schlechten Wetterbedingngen stornieren.
 Sie rufen in der zentralen Reservierung an, um zu schauen, in welchen Hotels 150 Fluggaeste fuer eine Nacht untergebracht werden koennen.
 Dazu muss an ueberpruefen, welche Zimmer gerade zur Verfuegung stehen.
 
@@ -28,7 +28,7 @@ Dazu muss an ueberpruefen, welche Zimmer gerade zur Verfuegung stehen.
 Ansonsten muss er weitere mit seiner Maschine einspeisen.
 
 8. Der Manager Herr 'Goldfish' moechte wissen, welche Kunden noch nicht bezahlt haben und wann diese Personen abreisen,
-ausserdem waee es noch gut, die Namen diese Kunden zu wissen.
+ausserdem waere es noch gut, die Namen diese Kunden zu wissen.
 
 9. Herr. 'Goldfish' moechte wissen, wie sich die offenen Posten zusammensetzen, allerdings schon zusammengefasst.
 
@@ -40,120 +40,106 @@ ausserdem waee es noch gut, die Namen diese Kunden zu wissen.
 
 /* 1. 'Witz-Garlton' und 'The Octopus' Manager fragt an*/
 
-	WITH Goldfish  as(
-		SELECT hotelid,gesamtumsatz, umsatzrooms, barumsatz,konsumumsatz,mietumsatz,benutzenumsatz
-		FROM HotelmanagerView
-		WHERE hotelid=3 
-
-		UNION
-
-		SELECT hotelid, gesamtumsatz, umsatzrooms, barumsatz,konsumumsatz,mietumsatz,benutzenumsatz
-		FROM HotelmanagerView
-		WHERE hotelid=4)
-	SELECT Goldfish.hotelid, hotelname, adresse, hoteltyp, gesamtumsatz, umsatzrooms, barumsatz,konsumumsatz,mietumsatz,benutzenumsatz
-	FROM Goldfish
-	JOIN hotel
-	on Goldfish.hotelid = hotel.hotelid;
+	WITH 	Goldfish  AS(
+	SELECT 	hotelid,gesamtumsatz, umsatzrooms, barumsatz,konsumumsatz,mietumsatz,benutzenumsatz
+	FROM 	HotelmanagerView
+	WHERE 	hotelid=3 
+	UNION
+	SELECT 	hotelid, gesamtumsatz, umsatzrooms, barumsatz,konsumumsatz,mietumsatz,benutzenumsatz
+	FROM 	HotelmanagerView
+	WHERE 	hotelid=4)
+	
+	SELECT 	Goldfish.hotelid, hotelname, adresse, hoteltyp, gesamtumsatz, umsatzrooms, barumsatz,konsumumsatz,mietumsatz,benutzenumsatz
+	FROM 	Goldfish
+	JOIN 	hotel ON Goldfish.hotelid = hotel.hotelid;
 
 
 /*2. Frau 'Putzfee' druckt ihre Liste aus */
 
-WITH KundenInHouse AS (SELECT *
-	FROM
-	reservierungen
-	WHERE gaestestatus='IN-HOUSE')
-	SELECT ReinigungspersonalView.gehoertzuhotel, ReinigungspersonalView.zimmernummer, grossputz, reserviertvonkunde, vorname, nachname
-	FROM ReinigungspersonalView
-	LEFT OUTER JOIN
-	KundenInHouse
-	ON Reinigungspersonalview.gehoertzuhotel = KundenInHouse.gehoertzuhotel and Reinigungspersonalview.zimmernummer = KundenInHouse.zimmer
-	LEFT OUTER JOIN
-	kunden
-	ON reserviertvonkunde = kunden.KID
-	WHERE Reinigungspersonalview.gehoertzuhotel=5;
+	WITH	KundenInHouse AS (
+	SELECT	*
+	FROM	reservierungen
+	WHERE 	gaestestatus='IN-HOUSE')
+	
+	SELECT 	ReinigungspersonalView.zimmernummer, grossputz, reserviertvonkunde, vorname, nachname
+	FROM 	ReinigungspersonalView
+		LEFT OUTER JOIN KundenInHouse
+		ON Reinigungspersonalview.gehoertzuhotel = KundenInHouse.gehoertzuhotel and Reinigungspersonalview.zimmernummer = KundenInHouse.zimmer
+		LEFT OUTER JOIN
+		kunden
+		ON reserviertvonkunde = kunden.KID
+	WHERE 	Reinigungspersonalview.gehoertzuhotel=5;
 
 /*3. Herr 'Gourmant' möchte es wissen.*/
 
-	SELECT hotelmanagerview.hotelid,hotelname, konsumumsatz
-	FROM hotelmanagerview
-	JOIN
-	hotel
-	ON hotelmanagerview.hotelid=hotel.hotelid
-	Where hotelmanagerview.hotelid=4;
+	SELECT 	hotelmanagerview.hotelid,hotelname, konsumumsatz
+	FROM 	hotelmanagerview
+		JOIN hotel
+		ON hotelmanagerview.hotelid=hotel.hotelid
+	WHERE	hotelmanagerview.hotelid=4;
 
-/*4. Feueralarm im 'The Shining'*/
+/*4. Feueralarm in 'The Shining'*/
 
-	SELECT bewohnteZimmerView.zimmernummer, bewohnteZimmerView.anreise, bewohnteZimmerView.abreise, bewohnteZimmerview.reserviertvonkunde, vorname, nachname
-	FROM bewohnteZimmerView
-	JOIN Reservierungen
-	ON bewohntezimmerview.gehoertzuhotel=reservierungen.gehoertzuhotel
-	AND bewohnteZimmerView.zimmernummer = reservierungen.zimmer
-	AND bewohntezimmerview.anreise = reservierungen.anreise
-	AND bewohntezimmerview.abreise = reservierungen.abreise
-	join kunden
-	on bewohntezimmerview.reserviertvonkunde = kid
-	WHERE bewohntezimmerview.gehoertzuhotel=6;
+	SELECT 	bewohnteZimmerView.zimmernummer, bewohnteZimmerView.anreise, bewohnteZimmerView.abreise, bewohnteZimmerview.reserviertvonkunde, 
+		vorname, nachname, Reservierungen.PersonenAnzahl AS GesamtPersonenImZimmer
+	FROM 	bewohnteZimmerView
+		JOIN Reservierungen
+		ON bewohntezimmerview.gehoertzuhotel=reservierungen.gehoertzuhotel
+		AND bewohnteZimmerView.zimmernummer = reservierungen.zimmer
+		AND bewohntezimmerview.anreise = reservierungen.anreise
+		AND bewohntezimmerview.abreise = reservierungen.abreise
+		JOIN kunden
+		ON bewohntezimmerview.reserviertvonkunde = kid
+	WHERE 	bewohntezimmerview.gehoertzuhotel=6;
 
 /*5. Welches Hotel*/
 
-	SELECT anreisendeview.gehoertzuhotel, anreisendeview.nachname, reserviertvonkunde, kunden.vorname, kunden.adresse
-	from
-	anreisendeview  
-	join
-	reservierungen
-	on anreisendeview.reservierungsnummer = reservierungen.reservierungsnummer
-	join
-	kunden
-	ON reserviertvonkunde = kid
-	WHERE anreisendeview.nachname = 'Hamilton';
+	SELECT 	Hotelname, Hotel.Adresse, anreisendeview.nachname, kunden.vorname, kunden.adresse
+	FROM 	anreisendeview  
+		JOIN Hotel ON anreisendeview.gehoertZuHotel = Hotel.HotelID
+		JOIN kunden ON anreisendeview.reserviertvonkunde = kid
+	WHERE 	anreisendeview.nachname = 'Hamilton';
 
 /*6. 150 Fluggaeste, aber wo unterbringen*/
 
-	SELECT freiezimmeraktuellview.hotelid, hotelname, adresse, hoteltyp,ezom,ezmm,dzom,dzmm,trom,trmm,suit
-	FROM 
-	freiezimmeraktuellview
-	join
-	hotel
-	on
-	freiezimmeraktuellview.hotelid = hotel.hotelid;
+	SELECT 	freiezimmeraktuellview.hotelid, hotelname, adresse, hoteltyp,ezom,ezmm,dzom,dzmm,trom,trmm,suit, Preis
+	FROM 	freiezimmeraktuellview
+		JOIN Hotel ON freiezimmeraktuellview.hotelid = hotel.hotelid
+		JOIN Preistabelle ON Hotel.hatPreisTabelle::text LIKE rtrim(CodeUndPosten,'-ABCDEFGHIJKLMNOPQRSTUVWXYZ')
+	-- bevorzuge billigste Einzelzimmer.
+	WHERE	'EZOM' LIKE ltrim(CodeUndPosten,'-0123456789') 
+	ORDER BY Preis DESC;
 
 /*7. Wieviele freie Karten haben wir noch?*/
 
-	SELECT counte(kartenid) as AnzahlKarten
-	from
-	freiekartenview;
+	SELECT 	count(kartenid) as AnzahlKarten
+	FROM	freiekartenview;
 
 /*8. Wer hat noch nicht bezahlt */
 
-	SELECT gehoertzuhotel,resa as Reservierungsnummer, kunde, vorname,nachname, anreise, abreise
-	from
-	Nichtbezahltkundenview
-	JOIN
-	kunden
-	on kunde = kid
-	WHERE gehoertzuhotel=3 or gehoertzuhotel=4;
+	SELECT 	gehoertzuhotel,resa as Reservierungsnummer, kunde, vorname,nachname, anreise, abreise
+	FROM	Nichtbezahltkundenview
+		JOIN kunden ON kunde = kid
+	WHERE 	gehoertzuhotel=3 or gehoertzuhotel=4;
 
 /*9. Wie sehen die Umsaetze der Kunden aus, die noch nicht bezahlt haben?*/
 
-	SELECT hotelid,reservierungsnummer,kunde,vorname, nachname,gesamtoffen,konsumiert,gemietet,benutzt,naechteumsatz
-	from unbezahltereservierungview
-	join KUNDEn
-	on kunde=kid
-	WHERE hotelid=3 or hotelid=4;
+	SELECT 	hotelid,reservierungsnummer,kunde,vorname, nachname,gesamtoffen,konsumiert,gemietet,benutzt,naechteumsatz
+	FROM	unbezahltereservierungview
+		JOIN Kunden
+	ON 	kunde=kid
+	WHERE 	hotelid=3 or hotelid=4;
+	-- Whoa, Jeremy Duncan
 
 /*10. Suche den VIP*/
 
-	SELECT bewohntezimmerview.gehoertzuhotel, zimmernummer, reserviertvonkunde, vip
-	from bewohntezimmerview
-	join
-	reservierungen
-	on bewohntezimmerview.zimmernummer = reservierungen.zimmer
-	and bewohntezimmerview.anreise = reservierungen.anreise
-	and bewohntezimmerview.abreise = reservierungen.abreise
-	and bewohntezimmerview.gehoertzuhotel = reservierungen.gehoertzuhotel
-	join
-	kunden
-	on reserviertvonkunde=kid
-	where bewohntezimmerview.gehoertzuhotel=3 and vip=true;
+	SELECT 	bewohntezimmerview.gehoertzuhotel, zimmernummer, bewohntezimmerview.reserviertvonkunde, vip
+	FROM	bewohntezimmerview
+		JOIN reservierungen ON bewohntezimmerview.zimmernummer = reservierungen.zimmer
+		AND bewohntezimmerview.anreise = reservierungen.anreise
+		AND bewohntezimmerview.abreise = reservierungen.abreise
+		AND bewohntezimmerview.gehoertzuhotel = reservierungen.gehoertzuhotel
+		JOIN kunden ON bewohntezimmerview.reserviertvonkunde=kid
+	WHERE	bewohntezimmerview.gehoertzuhotel=3 and vip=true;
 
 
