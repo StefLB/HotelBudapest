05464bc58cf8179579e76d3169bfc37c13ce467a
Elli <s9elpich@stud.uni-saarland.de>
2014-08-22 09:38:59 +0200

    vorschlaege

--- 8_Index.sql
+++ 8_Index.sql
@@ -18,41 +18,49 @@ Die groessten Daten sind:
 
 	CREATE INDEX index_reservierungen
 	ON reservierungen (reservierungsnummer);
+	-- hier ist vll. anreise oder abreise gut
 
 /* 2. Kunden */
 
 	CREATE INDEX index_kunden
 	ON kunden (kid);
+	-- hier ist vll. kunde nicht so geeignet
+
 
 /* 3. Speisen und Getraenke*/
 
 	CREATE INDEX index_speisenundgetraenke
 	on speisenundgetraenke (speiseid);
-
+	-- hier ist vll. wirdserviert in besser mit index auf gehoertzuhotel, aid
+	
 /* 4. Zimmer */
 
 	CREATE INDEX index_zimmer
 	on zimmer (gehoertzuhotel,zimmernummer);
+	-- hier ist vll. gehoertZuHotel,dreckig gut
+
 
 /* 5.bezahlen */
 	
 	CREATE INDEX index_bezahlen
 	on bezahlen (reservierungsnummer,kid,zeitpunkt);
+	-- hier ist vll. nur kid besser
 
 
 /* 6.benutzen */
 
 	CREATE INDEX index_benutzen
 	on benutzen (gehoertzuhotel,aid,kid,von);
-
+	-- hier ist vll. bewohntezimmerview gut mit index auf gehoertzuhotel
 
 /* 7.mieten */
 
 	CREATE INDEX index_mieten
 	on mieten (gehoertzuhotel,aid,kid,von,bis);
-
+	-- hier ist vll. nur kid besser
 
 /* 8.konsumieren */
 
 	CREATE INDEX index_konsumieren
 	on konsumieren (kid, zeitpunkt);
+	-- hier ist vll. nur kid besser
