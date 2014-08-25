9f4a99a12dfc8e485e811ca31965c10488d8d218
Elli <s9elpich@stud.uni-saarland.de>
2014-08-25 17:43:20 +0200

    doku

--- 7.2_Leseoperationen.sql
+++ 7.2_Leseoperationen.sql
@@ -1,38 +1,39 @@
-﻿/* LESEOPERATIONEN
+﻿/* 
+LESEOPERATIONEN
 
-10 Leseoperationen, vier davon keine skalaren Unteranfragen
+10 Leseoperationen, vier davon mit nicht skalaren Unteranfragen
 
 INHALTSANGABE
 
-1. Der Manager Herr 'Goldfish', der gleichzeitig zwei Hotels betreut, moechte vom 'Witz-Garlton', sowie vom 'The Octopus' den bis zum heutigen Tage angenommen Gesamtumsatz des 
-Hotels betrachten, aufgesplittet.
+	1. Der Manager Herr Goldfish, der gleichzeitig zwei Hotels betreut, moechte vom Witz-Garlton, sowie vom The Octopus 
+	   den bis zum heutigen Tage angenommen Gesamtumsatz des Hotels betrachten, aufgesplittet.
 
-2. Die leitende Hausdame, Frau 'Putzfee' vom 'BudgetInn' moechte ihre Listen fuer die Reinigungskraefte ausdrucken.
-Da sie moechte, dass ihre werten Kollegen wissen, wie die Gaeste heissen, moechte sie die Namen zu den 
-bewohnten Zimmer vermerkt haben.
+	2. Die leitende Hausdame, Frau Putzfee vom BudgetInn moechte ihre Listen fuer die Reinigungskraefte ausdrucken.
+	   Da sie moechte, dass ihre werten Kollegen wissen, wie die Gaeste heissen, moechte sie die Namen zu den 
+	   bewohnten Zimmer vermerkt haben, und eventuelle Wuensche der Gaeste.
 
-3. Der exzentrische Restaurationsleiter Herr 'Gourmant' moechte sich anschauen, wieviel bis jetzt in seinen
-Etablissements verzehrt wurde.
+	3. Der exzentrische Restaurationsleiter Herr Gourmant moechte sich anschauen, wieviel bis jetzt in seinen
+	   Etablissements verzehrt wurde.
 
-4. Ein Feueralarm bricht im Hotel 'The Shining' aus, schnell moechte sich die Rezeptionistin alle Daten 
-der Gaeste holen, die im Haus sind.
+	4. Ein Feueralarm bricht im Hotel The Shining aus, schnell moechte sich die Rezeptionistin alle Daten 
+	   der Gaeste holen, die im Haus sind.
 
-5. 'Welches Hotel habe ich heute nochmal gebucht', fragt die viel reisende Frau 'Hamilton' in der zentralen Reservierung nach.
-Bevor die Reservierungsmitabrieterin die Daten nennt, gleicht sie nochmal die persoenlichen Daten mit Frau 'Hamilton' ab.
+	5. 'Welches Hotel habe ich heute nochmal gebucht?', fragt die viel reisende Frau Hamilton in der zentralen Reservierung nach.
+	   Bevor die Reservierungsmitabrieterin die Daten nennt, gleicht sie nochmal die persoenlichen Daten mit Frau Hamilton ab.
 
-6. Das Flugzeug 'Boring Airline' musste seinen Flug aufgrund von schlechten Wetterbedingngen stornieren.
-Sie rufen in der zentralen Reservierung an, um zu schauen, in welchen Hotels 150 Fluggaeste fuer eine Nacht untergebracht werden koennen.
-Dazu muss an ueberpruefen, welche Zimmer gerade zur Verfuegung stehen.
+	6. Das Flugzeug Boring Airline GmbH musste seinen Flug aufgrund von schlechten Wetterbedingngen stornieren.
+	   Sie rufen in der zentralen Reservierung an, um zu schauen, in welchen Hotels 150 Fluggaeste fuer eine Nacht untergebracht werden koennen.
+	   Dazu muss an ueberpruefen, welche Zimmer gerade zur Verfuegung stehen.
 
-7. Brauchen wir weitere Karten im System, fragt sich die der Rooms-Divison Manager und schaut in das System, wieviel noch verfügbar sind.
-Ansonsten muss er weitere mit seiner Maschine einspeisen.
+	7. Brauchen wir weitere Karten im System, fragt sich die der Rooms-Divison Manager und schaut in das System, wieviel noch verfügbar sind.
+	   Ansonsten muss er weitere mit seiner Maschine einspeisen.
 
-8. Der Manager Herr 'Goldfish' moechte wissen, welche Kunden noch nicht bezahlt haben und wann diese Personen abreisen,
-ausserdem waere es noch gut, die Namen diese Kunden zu wissen.
+	8. Der Manager Herr Goldfish moechte wissen, welche Kunden noch nicht bezahlt haben und wann diese Personen abreisen,
+	   ausserdem waere es noch gut, die Namen diese Kunden zu wissen.
 
-9. Herr. 'Goldfish' moechte wissen, wie sich die offenen Posten zusammensetzen, allerdings schon zusammengefasst.
+	9. Herr. Goldfish moechte wissen, wie sich die offenen Posten zusammensetzen, allerdings schon zusammengefasst.
 
-10. VIPS bekommen im 'Witz-Garlton' jeden Tag einen frischen Fruechteteller auf das Zimmer gestellt, dazu muss ueberprueft werden, welche Gaeste VIPS sind.
+	10. VIPS bekommen im Witz-Garlton jeden Tag einen frischen Fruechteteller auf das Zimmer gestellt, dazu muss ueberprueft werden, welche Gaeste VIPS sind.
 
 
 
@@ -61,7 +62,7 @@ ausserdem waere es noch gut, die Namen diese Kunden zu wissen.
 	FROM	reservierungen
 	WHERE 	gaestestatus='IN-HOUSE')
 	
-	SELECT 	ReinigungspersonalView.zimmernummer, grossputz, reserviertvonkunde, vorname, nachname
+	SELECT 	ReinigungspersonalView.zimmernummer, grossputz, reserviertvonkunde, vorname, nachname, Wuensche
 	FROM 	ReinigungspersonalView
 		LEFT OUTER JOIN KundenInHouse
 		ON Reinigungspersonalview.gehoertzuhotel = KundenInHouse.gehoertzuhotel and Reinigungspersonalview.zimmernummer = KundenInHouse.zimmer
@@ -70,7 +71,7 @@ ausserdem waere es noch gut, die Namen diese Kunden zu wissen.
 		ON reserviertvonkunde = kunden.KID
 	WHERE 	Reinigungspersonalview.gehoertzuhotel=5;
 
-/*3. Herr 'Gourmant' möchte es wissen.*/
+/*3. Herr Gourmant moechte es wissen.*/
 
 	SELECT 	hotelmanagerview.hotelid,hotelname, konsumumsatz
 	FROM 	hotelmanagerview
@@ -78,7 +79,7 @@ ausserdem waere es noch gut, die Namen diese Kunden zu wissen.
 		ON hotelmanagerview.hotelid=hotel.hotelid
 	WHERE	hotelmanagerview.hotelid=4;
 
-/*4. Feueralarm in 'The Shining'*/
+/*4. Feueralarm in The Shining. */
 
 	SELECT 	bewohnteZimmerView.zimmernummer, bewohnteZimmerView.anreise, bewohnteZimmerView.abreise, bewohnteZimmerview.reserviertvonkunde, 
 		vorname, nachname, Reservierungen.PersonenAnzahl AS GesamtPersonenImZimmer
@@ -92,15 +93,18 @@ ausserdem waere es noch gut, die Namen diese Kunden zu wissen.
 		ON bewohntezimmerview.reserviertvonkunde = kid
 	WHERE 	bewohntezimmerview.gehoertzuhotel=6;
 
-/*5. Welches Hotel*/
+/*5. Welches Hotel nochmal? */
 
+	-- Falls 4.3.Beispielanfragen uebersprungen wurden
+	SELECT 	setArrivals();
+	
 	SELECT 	Hotelname, Hotel.Adresse, anreisendeview.nachname, kunden.vorname, kunden.adresse
 	FROM 	anreisendeview  
 		JOIN Hotel ON anreisendeview.gehoertZuHotel = Hotel.HotelID
 		JOIN kunden ON anreisendeview.reserviertvonkunde = kid
 	WHERE 	anreisendeview.nachname = 'Hamilton';
 
-/*6. 150 Fluggaeste, aber wo unterbringen*/
+/*6. 150 Fluggaeste, aber wo unterbringen? */
 
 	SELECT 	freiezimmeraktuellview.hotelid, hotelname, adresse, hoteltyp,ezom,ezmm,dzom,dzmm,trom,trmm,suit, Preis
 	FROM 	freiezimmeraktuellview
@@ -110,7 +114,7 @@ ausserdem waere es noch gut, die Namen diese Kunden zu wissen.
 	WHERE	'EZOM' LIKE ltrim(CodeUndPosten,'-0123456789') 
 	ORDER BY Preis DESC;
 
-/*7. Wieviele freie Karten haben wir noch?*/
+/*7. Wieviele freie Karten haben wir noch? */
 
 	SELECT 	count(kartenid) as AnzahlKarten
 	FROM	freiekartenview;
@@ -124,7 +128,7 @@ ausserdem waere es noch gut, die Namen diese Kunden zu wissen.
 
 /*9. Wie sehen die Umsaetze der Kunden aus, die noch nicht bezahlt haben?*/
 
-	SELECT 	hotelid,reservierungsnummer,kunde,vorname, nachname,gesamtoffen,konsumiert,gemietet,benutzt,naechteumsatz
+	SELECT 	hotelid,reservierungsnummer,kunde,vorname, nachname,gesamtbetrag,konsumiert,gemietet,benutzt,naechteumsatz
 	FROM	unbezahltereservierungview
 		JOIN Kunden
 	ON 	kunde=kid
